873106d0a391b0eb4597db42e74bd105e48de401
Haris Samingan <muhamad_haris@hotmail.com>
2022-07-20 15:58:01 +0800

    Add login screen and profile

---
+++ screens/LoginScreen.js
@@ -0,0 +1,79 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { PROFILE_SCREEN } from "./ProfileScreen";
+
+export default function LoginScreen() {
+  const navigation = useNavigation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login to your account</Text>
+
+      <TextInput
+        style={styles.inputView}
+        placeholder="Email"
+        value={username}
+        onChangeText={(username) => setUsername(username)}
+      />
+
+      <TextInput
+        style={styles.inputView}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(pw) => setPassword(pw)}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate(PROFILE_SCREEN);
+        }}
+      >
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export const LOGIN_SCREEN = "LOGIN_SCREEN";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 100,
+    padding: 25,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 40,
+    marginBottom: 50,
+  },
+  inputView: {
+    backgroundColor: "#F1F0F5",
+    borderRadius: 5,
+    marginBottom: 30,
+    padding: 20,
+  },
+  button: {
+    backgroundColor: "black",
+    borderRadius: 15,
+    width: "100%",
+  },
+  buttonText: {
+    textAlign: "center",
+    fontWeight: "400",
+    fontSize: 17,
+    padding: 20,
+    color: "white",
+  },
+});
